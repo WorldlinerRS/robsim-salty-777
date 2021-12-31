edaba01dec61e99966bf89d262c02610f64bfd4e
Robin Breitfeld <robin01196@gmail.com>
2021-12-31 12:59:56 +0100

    version 1.7

--- PackageSources/html_ui/Pages/VCockpit/Instruments/Airliners/CS777-300ER/VSpeed/CS777-300ER_VSpeed.js
+++ PackageSources/html_ui/Pages/VCockpit/Instruments/Airliners/CS777-300ER/VSpeed/CS777-300ER_VSpeed.js
@@ -1,7 +1,25 @@
 class B747_8_VSpeed extends Boeing_FCU.VSpeed {
+
+    constructor() {
+        super();
+        this.isInitialized = false;
+    }
     get templateID() { return "B747_8_VSpeed"; }
+    
     shouldBeVisible() {
         return SimVar.GetSimVarValue("L:AP_VS_ACTIVE", "number") === 1;
+    }
+
+    init() {
+        this.isInitialized = true;
+    }
+
+    update(_deltaTime) {
+        if (!this.isInitialized) {
+            return;
+        }
+
+
     }
 }
 registerInstrument("b747-8-vspeed-element", B747_8_VSpeed);
--- PackageSources/html_ui/Pages/VCockpit/Instruments/Airliners/CS777F/HDG/CS777F_HDG.js
+++ PackageSources/html_ui/Pages/VCockpit/Instruments/Airliners/CS777F/HDG/CS777F_HDG.js
@@ -1,5 +1,20 @@
 class B747_8_HDG extends Boeing_FCU.HDG {
+    
+    constructor() {
+        super();
+        this.isInitialized = false;
+    }
     get templateID() { return "B747_8_HDG"; }
+    
+    init() {
+        this.isInitialized = true;
+    }
+
+    update(_deltaTime) {
+        if (!this.isInitialized) {
+            return;
+        } 
+    }
 }
 registerInstrument("b747-8-hdg-element", B747_8_HDG);
 //# sourceMappingURL=B747_8_HDG.js.map
