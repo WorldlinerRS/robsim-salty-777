ab64d22ce7aaa666c7898c44dcfa38cf9486051c
Robin Breitfeld <robin01196@gmail.com>
2022-02-10 16:22:45 +0100

    modify IRS status query. Add local variable

--- PackageSources/html_ui/Pages/Salty/SaltyIRS.js
+++ PackageSources/html_ui/Pages/Salty/SaltyIRS.js
@@ -11,24 +11,35 @@ class SaltyIRS {
         if (this.lastTime == null) this.lastTime = timeNow;
         var deltaTime = timeNow - this.lastTime;
         this.lastTime = timeNow;
-
+        
         if (!electricityIsAvail) return;
 
         var IRSState = SimVar.GetSimVarValue("L:SALTY_IRS_STATE", "Enum");
+        var isIRSOn = SimVar.GetSimVarValue("L:B777_ADIRU_SWITCH_1", "Enum") >= 1;
+        var isSomeIRSOn = SimVar.GetSimVarValue("L:B777_ADIRU_SWITCH_1", "Enum") >= 1;
         SimVar.SetSimVarValue("L:SALTY_IRS_TIME_LEFT", "Enum", this.irsTimer);
 
-        if (IRSState != 0) {
-            SimVar.SetSimVarValue("L:SALTY_IRS_STATE", "Enum", 2);
+        if (!isSomeIRSOn && IRSState != 0) {
+            SimVar.SetSimVarValue("L:SALTY_IRS_STATE", "Enum", 0);
             IRSState = 0;
         }
 
-        if (IRSState == 0) {
-            SimVar.SetSimVarValue("L:SALTY_IRS_STATE", "Enum", 2);
+        if (isIRSOn && IRSState == 0) {
+            SimVar.SetSimVarValue("L:SALTY_IRS_STATE", "Enum", 1);
             IRSState = 1;
+
+            // irs "TIME TO ALIGN" in seconds, default = 7 * 60 ... reduce this to a lower number whilst debugging to protect sanity.
+            this.irsTimer = 7 * 60;
         }
 
         if (IRSState == 1) {
-            SimVar.SetSimVarValue("L:SALTY_IRS_STATE", "Enum", 2);
+            if (this.irsTimer > 0) {
+                this.irsTimer -= deltaTime / 1000;
+                if (this.irsTimer <= 0) {
+                    this.irsTimer = -1;
+                    SimVar.SetSimVarValue("L:SALTY_IRS_STATE", "Enum", 2);
+                }
+            }
         }
     }
 }
